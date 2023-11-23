9f83f0d57915da326d17713c8ddd213cae3ee00c
Magdalena Lonc <magdalena.lonc@gmail.com>
2023-11-23 19:22:41 +0100

    Add flaps and animations

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 import '../widgets/cat.dart';
@@ -13,19 +15,44 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
   late Animation<double> catAnimation;
   late AnimationController catController;
 
+  late Animation<double> boxAnimation;
+  late AnimationController boxController;
+
   @override
   void initState() {
     super.initState();
+
     catController = AnimationController(
-      duration: const Duration(seconds: 2),
+      duration: const Duration(milliseconds: 200),
       vsync: this,
     );
-    catAnimation = Tween(begin: 0.0, end: 100.0).animate(
+    catAnimation = Tween(begin: -35.0, end: -80.0).animate(
       CurvedAnimation(
         parent: catController,
         curve: Curves.easeIn,
       ),
     );
+
+    boxController = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      vsync: this,
+    );
+    // 'pi' comes from 'dart:math' library
+    boxAnimation = Tween(begin: pi * 0.6, end: pi * 0.65).animate(
+      CurvedAnimation(
+        parent: boxController,
+        curve: Curves.easeInOut,
+      ),
+    );
+    // to change multiple animations use 'addStatusListener()'
+    boxAnimation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        boxController.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        boxController.forward();
+      }
+    });
+    boxController.forward();
   }
 
   Widget buildCatAnimation() {
@@ -43,22 +70,70 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
     );
   }
 
-  void onTap() {
-    if (catController.status == AnimationStatus.completed) {
-      // reverse() method is opposite of forward(); form end value to begin
-      catController.reverse();
-    } else if (catController.status == AnimationStatus.dismissed) {
-      // starting animation method; from begin value to end
-      catController.forward();
-    }
-  }
-
   Widget buildBox() {
     return Container(
       height: 200.0,
       width: 200.0,
       color: Colors.brown,
     );
+  }
+
+  Widget buildLeftFlap() {
+    return Positioned(
+      top: 1.5,
+      left: 3.0,
+      child: AnimatedBuilder(
+        animation: boxAnimation,
+        builder: (context, child) {
+          return Transform.rotate(
+            // starting rotation point
+            alignment: Alignment.topLeft,
+            angle: boxAnimation.value, // -> These are not degrees, but RADIANS
+            child: child,
+          );
+        },
+        child: Container(
+          height: 10.0,
+          width: 125.0,
+          color: Colors.brown,
+        ),
+      ),
+    );
+  }
+
+  Widget buildRightFlap() {
+    return Positioned(
+      top: 1.5,
+      right: 3.0,
+      child: AnimatedBuilder(
+        animation: boxAnimation,
+        builder: (context, child) {
+          return Transform.rotate(
+            // starting rotation point
+            alignment: Alignment.topRight,
+            angle: -boxAnimation.value, // -> These are not degrees, but RADIANS
+            child: child,
+          );
+        },
+        child: Container(
+          height: 10.0,
+          width: 125.0,
+          color: Colors.brown,
+        ),
+      ),
+    );
+  }
+
+  void onTap() {
+    if (catController.status == AnimationStatus.completed) {
+      boxController.forward();
+      // reverse() method is opposite of forward(); form end value to begin
+      catController.reverse();
+    } else if (catController.status == AnimationStatus.dismissed) {
+      boxController.stop();
+      // starting animation method; from begin value to end
+      catController.forward();
+    }
   }
 
   @override
@@ -74,11 +149,14 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
         onTap: onTap,
         child: Center(
           child: Stack(
+            clipBehavior: Clip.none,
             children: [
               // 1st Widget in Stack is towards the very bottom
-              buildBox(),
-              // The last Widget in Stack is up the top
               buildCatAnimation(),
+              buildBox(),
+              // The last Widget in Stack is up the top, alignment by default to the top-left corner
+              buildLeftFlap(),
+              buildRightFlap(),
             ],
           ),
         ),
